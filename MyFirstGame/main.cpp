3a3c39e11daeeb65ccca1a78572f79d0db6cdafe
Andriy <andriy@intteh.com>
2018-10-17 19:36:23 +0300

    added namespace sf

--- MyFirstGame/main.cpp
+++ MyFirstGame/main.cpp
@@ -1,89 +1,65 @@
-
-//
-// Disclaimer:
-// ----------
-//
-// This code will work only if you selected window, graphics and audio.
-//
-// Note that the "Run Script" build phase will copy the required frameworks
-// or dylibs to your application bundle so you can execute it on any OS X
-// computer.
-//
-// Your resource files (images, sounds, fonts, ...) are also copied to your
-// application bundle. To get the path to these resources, use the helper
-// function `resourcePath()` from ResourcePath.hpp
-//
-
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
+using namespace sf;
+
 int main(int, char const**)
 {
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    RenderWindow window(VideoMode(800, 600), "SFML window");
 
-    // Set the Icon
-    sf::Image icon;
+    Image icon;
     if (!icon.loadFromFile(resourcePath() + "icon.png")) {
         return EXIT_FAILURE;
     }
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 
     // Load a sprite to display
-    sf::Texture texture;
+    Texture texture;
     if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
         return EXIT_FAILURE;
     }
-    sf::Sprite sprite(texture);
+    Sprite sprite(texture);
 
     // Create a graphical text to display
-    sf::Font font;
+    Font font;
     if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
         return EXIT_FAILURE;
     }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
+    Text text("Hello SFML", font, 50);
+    text.setFillColor(Color::Black);
 
-    // Load a music to play
-    sf::Music music;
+    Music music;
     if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
         return EXIT_FAILURE;
     }
 
-    // Play the music
     music.play();
 
-    // Start the game loop
     while (window.isOpen())
     {
-        // Process events
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
             // Close window: exit
-            if (event.type == sf::Event::Closed) {
+            if (event.type == Event::Closed) {
                 window.close();
             }
 
-            // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape) {
                 window.close();
             }
         }
 
-        // Clear screen
         window.clear();
 
-        // Draw the sprite
         window.draw(sprite);
 
-        // Draw the string
         window.draw(text);
 
-        // Update the window
         window.display();
     }
 
